680f7361511c8fbabe5b1bb6d0e69350379db694
Syamantak <Syamantak1305@gmail.com>
2026-03-10 22:29:08 +0530

    Functionalities were added in QuickKartRepository

--- Infosys.QuickKartDBFirst/Infosys.DBFirstCore.ConsoleApp/Program.cs
+++ Infosys.QuickKartDBFirst/Infosys.DBFirstCore.ConsoleApp/Program.cs
@@ -23,6 +23,52 @@ namespace Infosys.DBFirstCore.ConsoleApp
             {
                 Console.WriteLine("{0}\t\t{1}", category.CategoryId, category.CategoryName);
             }
+
+            byte categoryId = 1;
+            List<Product> lstProducts = repository.GetProductsOnCategoryId(categoryId);
+            if (lstProducts.Count == 0)
+            {
+                Console.WriteLine("No products available under the category = " + categoryId);
+            }
+            else
+            {
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                foreach (var product in lstProducts)
+                {
+                    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+                }
+            }
+
+            Product product1 = repository.FilterProducts(categoryId);
+            if (product1 == null)
+            {
+                Console.WriteLine("No product details available");
+            }
+            else
+            {
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product1.ProductId, product1.ProductName, product1.CategoryId, product1.Price, product1.QuantityAvailable);
+            }
+            Console.WriteLine();
+
+            string pattern = "BMW%";
+            List<Product> lstProducts1 = repository.FilterProductsUsingLike(pattern);
+            if (lstProducts1.Count == 0)
+            {
+                Console.WriteLine("No products available with the = " + pattern);
+            }
+            else
+            {
+                Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", "ProductId", "ProductName", "CategoryId", "Price", "QuantityAvailable");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+                foreach (var product in lstProducts1)
+                {
+                    Console.WriteLine("{0,-15}{1,-30}{2,-15}{3,-10}{4}", product.ProductId, product.ProductName, product.CategoryId, product.Price, product.QuantityAvailable);
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
--- Infosys.QuickKartDBFirst/Infosys.DBFirstCore.DataAccessLayer/QuickKartRepository.cs
+++ Infosys.QuickKartDBFirst/Infosys.DBFirstCore.DataAccessLayer/QuickKartRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Infosys.DBFirstCore.DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,5 +18,38 @@ namespace Infosys.DBFirstCore.DataAccessLayer
             List<Category> categories = context.Categories.OrderBy(c => c.CategoryId).ToList();
             return categories;
         }
+
+        public List<Product> GetProductsOnCategoryId(byte categoryId)
+        {
+            List<Product> lstProducts = context.Products.Where(p => p.CategoryId == categoryId).OrderBy(p => p.ProductId).ToList();
+            return lstProducts;
+        }
+        public Product FilterProducts(byte categoryId)
+        {
+            Product product = new Product();
+            try
+            {
+                product = context.Products.Where(p => p.CategoryId == categoryId).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                product = null;
+            }
+            return product;
+        }
+        public List<Product> FilterProductsUsingLike(string pattern)
+        {
+            List<Product> lstProduct = new List<Product>();
+            try
+            {
+                lstProduct = context.Products.Where(p => EF.Functions.Like(p.ProductName, pattern)).ToList();
+            }
+            catch (Exception ex)
+            {
+                lstProduct = null;
+            }
+            return lstProduct;
+        }
+
     }
 }
