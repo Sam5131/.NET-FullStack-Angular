7d8e9a9906eb61447f2f4339616b720319b47d44
Syamantak <Syamantak1305@gmail.com>
2026-03-15 12:17:29 +0530

    Categories And Product Classes added. Initial DB created

--- Code-first-approach/InfosysQuickKartCodeFirst/Infosys.CodeFirstCore.DataAccessLayer/Models/Category.cs
+++ Code-first-approach/InfosysQuickKartCodeFirst/Infosys.CodeFirstCore.DataAccessLayer/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Infosys.CodeFirstCore.DataAccessLayer.Models
@@ -11,6 +12,9 @@ namespace Infosys.CodeFirstCore.DataAccessLayer.Models
             Products = new HashSet<Product>();
         }
         public byte CategoryId { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string CategoryName { get; set; }
         public ICollection<Product> Products { get; set; }
     }
--- Code-first-approach/InfosysQuickKartCodeFirst/Infosys.CodeFirstCore.DataAccessLayer/Models/Product.cs
+++ Code-first-approach/InfosysQuickKartCodeFirst/Infosys.CodeFirstCore.DataAccessLayer/Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Infosys.CodeFirstCore.DataAccessLayer.Models
@@ -7,9 +9,20 @@ namespace Infosys.CodeFirstCore.DataAccessLayer.Models
     public class Product
     {
         public string ProductId { get; set; }
+
+        [Required]
+        [MinLength(0)]
+        [MaxLength(50)]
         public string ProductName { get; set; }
+
+        [Column(TypeName = "Numeric(8)")]
         public decimal Price { get; set; }
+
+        [ForeignKey("CategoryId")]
         public byte CategoryId { get; set; }
+
+        [Required]
+        [Range(minimum: 0, maximum: int.MaxValue)]
         public int QuantityAvailable { get; set; }
         public Category Category { get; set; }
     }
