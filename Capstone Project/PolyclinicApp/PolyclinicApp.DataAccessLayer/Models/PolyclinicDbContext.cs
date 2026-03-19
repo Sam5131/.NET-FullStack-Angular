e4b452037aa00f5dad2cebac3d06d6b49f8a6288
Syamantak <Syamantak1305@gmail.com>
2026-03-19 17:21:00 +0530

    Web Api project was added

--- Capstone Project/PolyclinicApp/PolyclinicApp.DataAccessLayer/Models/PolyclinicDbContext.cs
+++ Capstone Project/PolyclinicApp/PolyclinicApp.DataAccessLayer/Models/PolyclinicDbContext.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 
 namespace PolyclinicApp.DataAccessLayer.Models;
 
@@ -24,8 +25,19 @@ public partial class PolyclinicDbContext : DbContext
     public virtual DbSet<DoctorAppointment> DoctorAppointments { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDb;Initial Catalog=PolyclinicDB;Integrated Security=true");
+    {
+        var builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json");
+        var config = builder.Build();
+        var connectionString = config.GetConnectionString("PolyclinicDBConnectionString");
+        if (!optionsBuilder.IsConfigured)
+        {
+            // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
