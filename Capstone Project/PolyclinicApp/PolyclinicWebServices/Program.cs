b9ba3f77ddab0bb69daa431cbef55c511cd87c99
Syamantak <Syamantak1305@gmail.com>
2026-03-19 17:37:19 +0530

    Lined up Web API with capstone project of EF core

--- Capstone Project/PolyclinicApp/PolyclinicWebServices/Program.cs
+++ Capstone Project/PolyclinicApp/PolyclinicWebServices/Program.cs
@@ -1,34 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PolyclinicApp.DataAccessLayer.Models;
+using PolyclinicApp.DataAccessLayer;
 
-namespace PolyclinicWebServices
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<PolyclinicRepository>(new PolyclinicRepository(new PolyclinicDbContext(new DbContextOptions<PolyclinicDbContext>())));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
